1da92140fcef1dd31a656113329d5eb56155af27
liuhe <edz@liuhedeMacBook-Pro.local>
2020-12-25 17:38:51 +0800

    更新代码

--- com.house.property.web/src/components/head/Head/Head.vue
+++ com.house.property.web/src/components/head/Head/Head.vue
@@ -1,14 +1,16 @@
 <template>
       <div id="header">
-        <ul>
-          <li>买房</li>
-          <li>卖房</li>
-          <li>租房</li>
-          <li>出租</li>
-        </ul>
-        <div class="loginReg">
-          <svg-icon icon-class="head-icon"></svg-icon>
-          <i>登陆</i> / <i>注册</i>
+        <div class="inner">
+          <ul>
+            <li>买房</li>
+            <li>卖房</li>
+            <li>租房</li>
+            <li>出租</li>
+          </ul>
+          <div class="loginReg">
+            <svg-icon icon-class="head-icon"></svg-icon>
+            <i>登陆</i> / <i>注册</i>
+          </div>
         </div>
       </div>
 </template>
@@ -19,6 +21,44 @@
 </script>
 <style lang="less" >
 #header{
+  width: 100%;
+  height: 38px;
+  line-height: 38px;
+  background-color: #101d37;
+  font-size: 12px;
+  .inner{
+    width: 1200px;
+    margin: 0 auto;
+    overflow: hidden;
+    ul{
+      width: 70%;
+      float: left;
+      li{
+        display: inline;
+        color: #a9abab;
+        margin: 0 20px;
+        
+      }
+      li:hover{
+        cursor: pointer;
+        color: rgb(228, 228, 228);
+      }
+    }
+    .loginReg{
+      width: 30%;
+      float: right;
+      color: rgb(228, 228, 228);
+      text-align: right;
+      i{
+        font-style: normal;
+        cursor: pointer;
+      }
+      .svg-icon{
+        width: 1.3em;
+        height: 1.3em;
+      }
 
+    }
+  }
 }
 </style>
--- com.house.property.web/src/components/house/HouseEdit.vue
+++ com.house.property.web/src/components/house/HouseEdit.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="houseEdit">
     <Head></Head>
+    <div class="title">
+      <div class="title-inner">
+        <h1>三桥地铁口，华润央企大楼盘高品质，刚需楼盘欢饮咨询</h1>
+        <p>地铁公交站、教育、户型好、视野开阔、楼间距大</p>
+      </div>
+    </div>
+    <div class="content">
+      <div class="content-top">
+        <div class="fl">
+          <img src="https://ke-image.ljcdn.com/110000-inspection/apc_ib7f9jRSU_1.jpg!m_fill,w_710,h_400,lg_north_west,lx_0,ly_0,l_fbk,f_jpg,ls_50?from=ke.com">
+        </div>
+        <div class="fr">
+          <div class="value">
+            <span>154.5</span>万
+          </div>
+          
+          
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,8 +33,62 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 #houseEdit{
+  .title{
+    width: 100%;
+    height: 150px;
+    background-color: #f5f5f6;
+    .title-inner{
+      width: 1200px;
+      margin: 0 auto;
+      h1{
+        font-size: 26px;
+        color: #101d37;
+        font-weight: 700;
+        line-height: 40px;
+        padding-top: 40px;
+      }
+      p{
+        font-size: 14px;
+        color: #aaa;
+        line-height: 20px;
+        padding-top: 10px;
+      }
+    }
+  }
+  .content{
+    width: 1200px;
+    margin: 0 auto;
+    .content-top{
+      width: 100%;
+      height: 400px;
+      overflow: hidden;
+      .fl{
+        width: 700px;
+        height: 400px;
+        float: left;
+      }
+      .fr{
+        width: 450px;
+        float: right;
+        padding-left: 50px;
+        .value{
+          padding: 30px 0;
+          font-size: 16px;
+          color: #e4393c;
+          span{
+            font-family: Tahoma;
+            font-size: 42px;
+            line-height: 42px;
+            color: #fe615a;
+            font-weight: 700;
+          }
+          
+        }
+      }
+    }
+  }
 
 }
 </style>
