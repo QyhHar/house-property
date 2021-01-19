5639a758894b5d09e0ef1b6039aeb04eb0f501fc
QyhHar <1458100138@qq.com>
2021-01-19 17:45:41 +0800

    1

--- com.house.property.web/config/index.js
+++ com.house.property.web/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
     proxyTable: {
       '/api': {
         // target: 'http://118.178.179.111:80',
-        target: 'http://127.0.0.1:8080',
+        target: 'http://127.0.0.1:8090',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
@@ -30,7 +30,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
